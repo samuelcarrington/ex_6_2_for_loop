d728afa85e736847901d38a7a5bbd2ffcc97241d
Samuel Carrington <samuelcarrington@gmail.com>
2016-01-15 14:15:30 -0500

    commit

---
+++ ex_6_2_for_loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_6_2_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*6-2  Write a program that reads from the console a series 
+            of integers and prints the smallest and largest of them. 
+            (Chapter 6, Exercise 3)
+            */
+        }
+    }
+}
